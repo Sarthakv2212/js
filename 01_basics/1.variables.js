e1e6a373d13f6810aabe96d14c5c63f5f841a0be
Sarthakv2212 <140988067+Sarthakv2212@users.noreply.github.com>
2024-03-10 23:17:18 +0000

    datatypes

---
+++ 01_basics/1.variables.js
@@ -0,0 +1,12 @@
+const accountid =45
+let accountemail="sarthakvarshney92@gmail.com"
+var accountpass = "12345"  //had scope problem so dont use
+let accountcity="delhi"    //this also works in js ie without declaration.But dont use.
+let accountState; //this will be shown as undefined.
+// accountid =2 this wont work as value is const . it cannot be changed.
+
+accountemail="djfbjef"
+
+console.log(accountpass)
+
+console.table([accountid,accountemail,accountcity,accountState   ])
---
+++ 01_basics/2.dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";   // treat all js code as newer version by the engine
+
+// alert(3+3) //since we are using node js not browser,it wont work here
+
+
+let name = "hitesh" //string
+let age = 18.        //number
+let isLoggedIn = false //boolean
+let state;  //undefined
+
+// number => 2 to power 53 (range)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+//null and undefined are different.null can be assigned to a variable to show a o value or non existant value whereas undefined is no value assigned.
+
+// symbol => unique
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object.  //interview question
+
