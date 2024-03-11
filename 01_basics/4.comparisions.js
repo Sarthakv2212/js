b934817b7039036d0ade14358ff8207ff3047ae5
Sarthakv2212 <140988067+Sarthakv2212@users.noreply.github.com>
2024-03-11 01:33:49 +0000

    comparisons done

---
+++ 01_basics/4.comparisions.js
@@ -0,0 +1,22 @@
+//simple ones
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//complex
+// console.log("2" > 1);  =true
+// console.log("02" > 1);  =true //zyadatar aise comparisions predictable nhi hote so be cautious both are numbers.
+
+console.log(null > 0);   //false
+console.log(null == 0);   //false
+console.log(null >= 0);  //true.  //pata nhi kya hai bas in sab == >= ki working alag hai...kutch conversion ka chakkr hai.
+
+console.log(undefined == 0);   
+console.log(undefined > 0);
+console.log(undefined < 0);
+//zyada dimaag khraab na kare ispe.
+// === 
+
+console.log("2" === 2);
